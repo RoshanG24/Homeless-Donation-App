c065ac699607e5a367f2ef8bacd249c2e058b50e
Roshan <roshangorasia@hotmail.com>
2019-01-20 21:38:51 +0000

    updated reg and login

--- app/src/main/java/com/example/homelessapp/Login.java
+++ app/src/main/java/com/example/homelessapp/Login.java
@@ -2,6 +2,7 @@ package com.example.homelessapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -35,23 +36,24 @@ public class Login extends AppCompatActivity {
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Handler handler = new Handler();
                 final String username = etUsername.getText().toString();
                 final String password = etPassword.getText().toString();
                 progressBar.setVisibility(View.VISIBLE);
-                Intent intent;
+                final Intent intent;
 
                 for (int i = 0; i < users.size(); i++) {
                     User user = users.get(i);
-                    try {
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
                     if (username.equals(user.getUserName()) && password.equals(user.getPassword())) {
                         intent = new Intent(Login.this, Dashboard.class);
                         intent.putExtra("username", username);
-                        Login.this.startActivity(intent);
-                        progressBar.setVisibility(View.INVISIBLE);
+
+                        handler.postDelayed(new Runnable() {
+                            public void run() {
+                                Login.this.startActivity(intent);
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+                        }, 2000);
                         break;
                     } else {
                         AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
--- app/src/main/java/com/example/homelessapp/Registration.java
+++ app/src/main/java/com/example/homelessapp/Registration.java
@@ -1,7 +1,15 @@
 package com.example.homelessapp;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.homelessapp.Database.User;
+
+import static com.example.homelessapp.Database.SampleDataProvider.users;
 
 public class Registration extends AppCompatActivity {
 
@@ -9,5 +17,18 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        final Button register_button =  findViewById(R.id.button);
+
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(Registration.this, Dashboard.class);
+                intent.putExtra("username", "bob");
+                Registration.this.startActivity(intent);
+            }
+        });
     }
 }
