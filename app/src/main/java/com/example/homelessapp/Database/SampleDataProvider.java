bb0113427e6ce7c65d38640644b922f2d0b8c47a
Mohannad Zeido <mohannad.zeido@gmail.com>
2019-01-20 23:09:26 +0000

    Completed homeless profile

--- app/src/main/java/com/example/homelessapp/Database/SampleDataProvider.java
+++ app/src/main/java/com/example/homelessapp/Database/SampleDataProvider.java
@@ -18,8 +18,11 @@ public class SampleDataProvider {
         pots.add("Clothing");
 
         homelessPeople = new ArrayList<>();
-        homelessPeople.add(new HomelessPerson( 1234,"Frank",  "Samwise", 23, "Hello, I am homeless and need your help please pllease ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help pleaello, I am homeless and need your help plea  ello, I am homeless and need your help plea ello, I am homeless and need your help pleaello, I am homeless and need your help plea", "Fly a kite" , "Reading, Uk", "1234.jpeg", pots));
-        homelessPeople.add(new HomelessPerson(4321,"Beth",  "Smith", 40, "I am temporarily homeless please", "Get a job","London, UK",  "4321.jpeg", pots));
+//        homelessPeople.add(new HomelessPerson( 1234,"Frank",  "Samwise", 23, "Hello, I am homeless and need your help please pllease ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help pleaello, I am homeless and need your help plea  ello, I am homeless and need your help plea ello, I am homeless and need your help pleaello, I am homeless and need your help plea", "Fly a kite" , "Reading, Uk", "1234.jpeg", pots));
+//        homelessPeople.add(new HomelessPerson(4321,"Beth",  "Smith", 40, "I am temporarily homeless please", "Get a job","London, UK",  "4321.jpeg", pots));
+
+        homelessPeople.add(new HomelessPerson( 1234,"Frank",  "Samwise", 23, "Hello, I am homeless and need your help please pllease ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help plea ello, I am homeless and need your help pleaello, I am homeless and need your help plea  ello, I am homeless and need your help plea ello, I am homeless and need your help pleaello, I am homeless and need your help plea", "Fly a kite" , "Reading, Uk", "p1234", pots));
+        homelessPeople.add(new HomelessPerson(4321,"Beth",  "Smith", 40, "I am temporarily homeless please", "Get a job","London, UK",  "p4321", pots));
 
         donations = new ArrayList<>();
         donations.add(new Donation(10, "Love you", "bob", "1234", "Food"));
--- app/src/main/java/com/example/homelessapp/HomelessProfile.java
+++ app/src/main/java/com/example/homelessapp/HomelessProfile.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.homelessapp.Database.HomelessPerson;
@@ -23,7 +26,7 @@ public class HomelessProfile extends AppCompatActivity {
         Intent intent = getIntent();
         int id = intent.getIntExtra("HOMELESS-ID", -1);
         if(id == -1){
-            id = 1234; //todo fix this please
+            id = 4321; //todo fix this please
         }
         for (HomelessPerson person: SampleDataProvider.homelessPeople) {
             if(id == person.getId()){
@@ -31,6 +34,11 @@ public class HomelessProfile extends AppCompatActivity {
                 break;
             }
         }
+
+        ImageView profileImage = findViewById(R.id.profile_image);
+        int imageId = getResources().getIdentifier("drawable/" + homelessPerson.getPicture(), null, getPackageName());
+        profileImage.setImageResource(imageId);
+
         TextView tv = findViewById(R.id.about_content);
         tv.setText(homelessPerson.getBiography());
         tv.setMovementMethod(new ScrollingMovementMethod());
@@ -38,5 +46,28 @@ public class HomelessProfile extends AppCompatActivity {
         TextView tv2 = findViewById(R.id.life_goals_content);
         tv2.setText(homelessPerson.getLifeGoals());
         tv2.setMovementMethod(new ScrollingMovementMethod());
+
+        TextView firstName = findViewById(R.id.first_name);
+        firstName.setText(homelessPerson.getFirstName());
+
+        TextView lastName = findViewById(R.id.last_name);
+        lastName.setText(homelessPerson.getLastName());
+
+        TextView location = findViewById(R.id.location);
+        location.setText(homelessPerson.getLocation());
+
+
+        Button donateButton = findViewById(R.id.donate);
+
+        final int idToSend = id;
+        donateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomelessProfile.this, DonationPots.class);
+                intent.putExtra("HOMELESS-ID", idToSend);
+                startActivity(intent);
+            }
+        });
+
     }
 }
