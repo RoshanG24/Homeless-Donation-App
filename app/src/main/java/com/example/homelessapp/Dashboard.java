1dd21f54329f3dd7c7fe97fe486fdc837f45d2b1
FrancisALR <francis.rooney95@gmail.com>
2019-01-20 16:49:38 +0000

    BG on dashboard

--- app/src/main/java/com/example/homelessapp/Dashboard.java
+++ app/src/main/java/com/example/homelessapp/Dashboard.java
@@ -23,13 +23,12 @@ public class Dashboard extends AppCompatActivity {
         pieChartView = findViewById(R.id.chart);
 
         List pieData = new ArrayList<>();
-        pieData.add(new SliceValue(40, Color.BLUE).setLabel("Food: £10"));
-        pieData.add(new SliceValue(30, Color.GRAY).setLabel("Rent: £4"));
-        pieData.add(new SliceValue(30, Color.RED).setLabel("Clothes: £18"));
+        pieData.add(new SliceValue(40, Color.parseColor("#E0E0E0")).setLabel("Food: £10"));
+        pieData.add(new SliceValue(30, Color.parseColor("#E0E0E0")).setLabel("Rent: £4"));
+        pieData.add(new SliceValue(30, Color.parseColor("#E0E0E0")).setLabel("Clothes: £18"));
 
         PieChartData pieChartData = new PieChartData(pieData);
         pieChartData.setHasLabels(true).setValueLabelTextSize(14);
-        pieChartData.setHasCenterCircle(true).setCenterText1("Sales in million").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
         pieChartView.setPieChartData(pieChartData);
 
     }
