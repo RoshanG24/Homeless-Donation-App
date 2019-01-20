088e59b6244e2b800a7b7b721581e5855b1a2056
Roshan <roshangorasia@hotmail.com>
2019-01-20 19:04:31 +0000

    Added login func

--- app/src/main/java/com/example/homelessapp/Login.java
+++ app/src/main/java/com/example/homelessapp/Login.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import com.example.homelessapp.Database.SampleDataProvider;
 import com.example.homelessapp.Database.User;
+
+import java.util.ArrayList;
+
+import static com.example.homelessapp.Database.SampleDataProvider.users;
 
 public class Login extends AppCompatActivity {
 
@@ -21,30 +26,40 @@ public class Login extends AppCompatActivity {
         final EditText etUsername = findViewById(R.id.username);
         final EditText etPassword = findViewById(R.id.password);
         final ProgressBar progressBar = findViewById(R.id.loginProgressBar);
+        final Button login_button =  findViewById(R.id.login_button);
+
         progressBar.setVisibility(View.INVISIBLE);
 
 
 
-        /*bLogin.setOnClickListener(new View.OnClickListener() {
+        login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String username = etUsername.getText().toString();
                 final String password = etPassword.getText().toString();
                 progressBar.setVisibility(View.VISIBLE);
-                Intent intent
-                    if (username.equals(##) && password.equals(##)) {
-                                intent = new Intent(Login.this, Dashboard.class);
-                                intent.putExtra("username", username);
-                                Login.this.startActivity(intent);
-                            } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
-                                builder.setMessage("Wrong username or password")
-                                        .setNegativeButton("Retry", null)
-                                        .create()
-                                        .show();
-                            }
-                        progressBar.setVisibility(View.INVISIBLE);
+                Intent intent;
+
+                for (int i = 0; i < users.size(); i++) {
+                    User user = users.get(i);
+                    if (username.equals(user.getUserName()) && password.equals(user.getPassword())) {
+                        intent = new Intent(Login.this, Dashboard.class);
+                        intent.putExtra("username", username);
+                        Login.this.startActivity(intent);
+                        break;
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                        builder.setMessage("Wrong username or password")
+                                .setNegativeButton("Retry", null)
+                                .create()
+                                .show();
                     }
-    }*/
-}
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+            }
+            });
+                }
+
+
+
 }
