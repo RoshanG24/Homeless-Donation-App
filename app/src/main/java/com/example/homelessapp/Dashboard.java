96718b4b5f25e24c069a9cf0b93302fb55895bfc
FrancisALR <francis.rooney95@gmail.com>
2019-01-20 15:33:55 +0000

    Add pie chart mock

--- app/src/main/java/com/example/homelessapp/Dashboard.java
+++ app/src/main/java/com/example/homelessapp/Dashboard.java
@@ -1,13 +1,41 @@
 package com.example.homelessapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
+
+
+
+import java.util.ArrayList;
 
 public class Dashboard extends AppCompatActivity {
+
+    PieChartView pieChartView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        pieChartView = findViewById(R.id.chart);
+
+        List pieData = new ArrayList<>();
+        pieData.add(new SliceValue(15, Color.BLUE).setLabel("Q1: $10"));
+        pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
+        pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
+        pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));
+
+        PieChartData pieChartData = new PieChartData(pieData);
+        pieChartData.setHasLabels(true).setValueLabelTextSize(14);
+        pieChartData.setHasCenterCircle(true).setCenterText1("Sales in million").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
+        pieChartView.setPieChartData(pieChartData);
+
     }
 }
