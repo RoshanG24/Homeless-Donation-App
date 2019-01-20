4c767a286d44e4ba025fe000788e0653f30b4168
Mohannad Zeido <mohannad.zeido@gmail.com>
2019-01-20 15:22:28 +0000

    Added the hardcoded data as an array

--- app/src/main/java/com/example/homelessapp/Database/SampleDataProvider.java
+++ app/src/main/java/com/example/homelessapp/Database/SampleDataProvider.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class SampleDataProvider {
     public static ArrayList<HomelessPerson> homelessPeople;
     public static ArrayList<String> pots;
+    public static ArrayList<User> users;
 
     static {
 
@@ -16,8 +17,10 @@ public class SampleDataProvider {
         pots.add("Clothing");
 
         homelessPeople = new ArrayList<>();
-        homelessPeople.add(new HomelessPerson("Frank Samwise", 23, "Hello, I am homeless and need your help please pllease", pots));
-        homelessPeople.add(new HomelessPerson("Frank Samwise", 23, "Hello, I am homeless and need your help please pllease", pots));
+        homelessPeople.add(new HomelessPerson( 1234,"Frank",  "Samwise", 23, "Hello, I am homeless and need your help please pllease", "Reading, Uk", null, pots));
+        homelessPeople.add(new HomelessPerson(4321,"Beth",  "Smith", 40, "I am temporarily homeless please", "London, UK",  null, pots));
 
+        users = new ArrayList<>();
+        users.add(new User("BobALob", "password", "bob@gmail.com"));
     }
 }
