2f1cb1e2682e8d64023e7e6dc0aac024368572e2
Roshan <roshangorasia@hotmail.com>
2019-01-20 15:43:04 +0000

    started login logic

--- app/src/main/java/com/example/homelessapp/Login.java
+++ app/src/main/java/com/example/homelessapp/Login.java
@@ -1,7 +1,15 @@
 package com.example.homelessapp;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +17,32 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+
+        final EditText etUsername = findViewById(R.id.username);
+        final EditText etPassword = findViewById(R.id.password);
+        final ProgressBar progressBar = findViewById(R.id.loginProgressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+       /* bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+                progressBar.setVisibility(View.VISIBLE);
+                Intent intent
+                            if (username.equals(##) && password.equals(##)) {
+                                intent = new Intent(Login.this, Dashboard.class);
+                                intent.putExtra("name", username);
+                                Login.this.startActivity(intent);
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                                builder.setMessage("Wrong username or password")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        progressBar.setVisibility(View.INVISIBLE);
+                    }
+    }*/
+}
 }
